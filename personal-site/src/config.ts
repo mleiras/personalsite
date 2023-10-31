35d0349a5f6d3d31a50836d2cd562aea9e811643
Monica Leiras <76125909+mleiras@users.noreply.github.com>
2023-10-31 15:55:03 +0000

    Update config.ts

--- personal-site/src/config.ts
+++ personal-site/src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Monica Leiras";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
+  "Welcome to my site! I'm a junior developer.";
 export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+export const MY_NAME = "Monica Leiras";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
